ca35837fbc1b9beab6e2a10dc9ad2eea82af918e
Shendryk <stanislav.shendryk@gmail.com>
2021-04-12 17:00:23 +0300

    feat: implement task 2,8,10,11,15

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -1,16 +1,22 @@
 /**
- * Given two strings, find the number of common characters between them.
+* Учитывая две строки, найдите количество общих персонажей между ними.
  *
  * @param {String} s1
  * @param {String} s2
  * @return {Number}
  *
  * @example
- * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
- * Strings have 3 common characters - 2 "a"s and 1 "c".
+* Для s1 = "aabcc" и s2 = "adcaa", выход должен быть 3
+ * Строки имеют 3 общих персонажа - 2 «A» и 1 «C».
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new Error('Not implemented');
+function getCommonCharacterCount (s1, s2) {
+  let counter = 0;
+  for (let i = 0; i < s1.length; i += 1) {
+    if (s2.indexOf(s1[i]) !== -1) {
+      counter += 1;
+    }
+  }
+  return counter - 1;
 }
 
 module.exports = getCommonCharacterCount;
--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -1,15 +1,19 @@
-/**
- * Given a string, return its encoding version.
- *
- * @param {String} str
- * @return {String}
- *
- * @example
- * For aabbbc should return 2a3bc
- *
- */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let counter = 1;
+  let result = '';
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      counter += 1;
+    } else if (counter >= 2) {
+      result += counter + str[i];
+      counter = 1;
+    } else {
+      result += str[i];
+      counter = 1;
+    }
+  }
+  return result;
 }
 
 module.exports = encodeLine;
